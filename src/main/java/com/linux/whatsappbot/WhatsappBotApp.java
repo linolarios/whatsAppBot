f3d346bfe2706269d7b00c6e4fb4a66cba57be18
Lino Larios <llarios@skytouchtechnology.com>
2021-01-25 20:47:27 -0600

    add main class

---
+++ src/main/java/com/linux/whatsappbot/WhatsappBotApp.java
@@ -0,0 +1,11 @@
+package com.linux.whatsappbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsappBotApp {
+    public static void main(String[] args) {
+        SpringApplication.run(WhatsappBotApp.class, args);
+    }
+}
