70cd1e50caa92818e3c1d318180d488a2648ed5b
Lino Larios <llarios@skytouchtechnology.com>
2021-01-25 22:53:12 -0600

    Refactor

--- src/main/java/com/linux/whatsappbot/controller/MessageController.java
+++ src/main/java/com/linux/whatsappbot/controller/MessageController.java
@@ -15,26 +15,16 @@ public class MessageController {
 
             String option = message.getBody().split(" ")[0].toLowerCase();
             switch (option) {
-                case "chatid":
-                    ApiWA.sendChatId(message.getChatId());
-                    break;
-                case "file":
+                case "chatid" -> ApiWA.sendChatId(message.getChatId());
+                case "file" -> {
                     final String[] texts = message.getBody().split(" ");
                     if (texts.length > 1)
                         ApiWA.sendFile(message.getChatId(), texts[1]);
-                    break;
-                case "ogg":
-                    ApiWA.sendOgg(message.getChatId());
-                    break;
-                case "geo":
-                    ApiWA.sendGeo(message.getChatId());
-                    break;
-                case "group":
-                    ApiWA.createGroup(message.getAuthor());
-                    break;
-                default:
-                    ApiWA.sendDefault(message.getChatId());
-                    break;
+                }
+                case "ogg" -> ApiWA.sendOgg(message.getChatId());
+                case "geo" -> ApiWA.sendGeo(message.getChatId());
+                case "group" -> ApiWA.createGroup(message.getAuthor());
+                default -> ApiWA.sendDefault(message.getChatId());
             }
 
         }
